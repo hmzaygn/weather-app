39c98e008317fa2dab31c04d43526025bf82faf1
Feanor <hamzaaygan1995@gmail.com>
2022-09-26 20:34:01 +0300

    weatherapp minor fixes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const input = document.getElementById("input");
 const button = document.getElementById("btn");
 const result = document.getElementById("result-div");
 const message = document.getElementById("message");
+const body = document.querySelector("body");
 let cityList = [];
 
 // const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value},tr&appid=05c2eab785b195a0d8ecc20b303f2cc0&units=metric`;
@@ -10,15 +11,16 @@ button.addEventListener("click", () => {
   if (!input.value.trim()) {
     message.innerHTML = "You have to enter a city";
     input.value = "";
+  } else if (cityList.includes(input.value)) {
+    message.innerHTML = "You already entered this city";
+    input.value = "";
   } else {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${input.value},tr&appid=05c2eab785b195a0d8ecc20b303f2cc0&units=metric`
     )
       .then((res) => {
         if (!res.ok) {
-          throw new Error("Something went wrong");
-        } else if (cityList.includes(input.value)) {
-          throw new Error("You already entered this city");
+          throw new Error("You have enter a valid city in Turkiye");
         }
         return res.json();
       })
@@ -29,6 +31,19 @@ button.addEventListener("click", () => {
   }
 });
 
+result.addEventListener("click", (e) => {
+  if ((e.target.classList = "fa-x")) {
+    e.target.parentElement.parentElement.remove();
+    cityList = cityList.filter(
+      (el) =>
+        el !=
+        e.target.parentElement.nextElementSibling.textContent.toLocaleLowerCase(
+          "tr"
+        )
+    );
+  }
+});
+
 const updateDom = (data) => {
   const {
     main: { temp },
@@ -36,6 +51,7 @@ const updateDom = (data) => {
   } = data;
   result.innerHTML += `
     <div class="weather-div">
+        <button id="cross" class="btn-cross"><i class="fa-solid fa-x"></i></button>
         <p class="city">${input.value.toLocaleUpperCase("tr")}</p>
         <p class="weather-degree"><span class="span">${Math.round(
           temp
@@ -46,9 +62,28 @@ const updateDom = (data) => {
         )}</p>
     </div>
   `;
+
+  if (weather[0].description == "clear sky") {
+    body.className = "sunny";
+  } else if (
+    weather[0].description == "few clouds" ||
+    weather[0].description == "scattered clouds" ||
+    weather[0].description == "broken clouds" ||
+    weather[0].description == "mist"
+  ) {
+    body.className = "cloudy";
+  } else if (
+    weather[0].description == "shower rain" ||
+    weather[0].description == "rain" ||
+    weather[0].description == "thunderstorm"
+  ) {
+    body.className = "rainy";
+  } else if (weather[0].description == "snow") {
+    body.className = "snowy";
+  }
+
   cityList.push(input.value);
   input.value = "";
-  console.log(temp, weather[0].description);
 };
 
 input.addEventListener("keydown", (e) => {
